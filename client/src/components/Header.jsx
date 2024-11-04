4807652e554bdf0084580a5aa865cf244bec1a53
gvfrontend <gk.2023.krish@gmail.com>
2024-11-04 15:30:55 +0530

    Create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
                 <FaMoon/>
              </Button> 
              <Link to='/sign-in'>
-              <Button gradientDuoTone='purpleToPink'>
+              <Button gradientDuoTone='purpleToPink' outline>
                 Sign In
               </Button>
              </Link>     
